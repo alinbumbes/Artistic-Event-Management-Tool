e911b606b9566396ec62769717a10cda0ad3bdff
Alin Bumbes <alin.bumbes@jlg.ro>
2013-12-06 18:35:01 +0200

    song validator added

---
+++ Core/Domain/Validation/SongValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace Core.Domain.Validation
+{
+    public class SongValidator : AbstractValidator<Song> 
+    {
+        public SongValidator()
+        {
+            RuleFor(song => song.Name)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}
